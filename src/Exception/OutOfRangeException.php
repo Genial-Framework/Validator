cc7b82fdc67f1827380a835e3f147ff175f3c421
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-12 15:26:45 -0500

    Add files via upload

---
+++ src/Exception/OutOfRangeException.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Validator> for the canonical source repository
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Validator/blob/master/LICENSE> New BSD License
+ */
+ 
+namespace Genial\Validator\Exception;
+
+/**
+ * OutOfRangeException
+ */
+class OutOfRangeException extends \OutOfRangeException implements ExceptionInterface
+{
+}
