fb8c52aefc21f640b95ca812ed177a9ec82f13f8
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-03 00:38:07 -0500

    Update ValidatorInterface.php

--- src/Use/ValidatorInterface.php
+++ src/Use/ValidatorInterface.php
@@ -1,1 +1,33 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Validator> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Validator/blob/master/LICENSE> New BSD License.
+ */
+
+namespace Genial\Validator\Use;
+
+/**
+ * ValidatorInterface.
+ */
+interface ValidatorInterface
+{
+ 
+    /**
+     * __invoke().
+     *
+     * Set options for proper use.
+     *
+     * @param array|[] $options The validator options.
+     *
+     * @return void.
+     */
+    public function __invoke(array $options = []);
+  
+}
 
