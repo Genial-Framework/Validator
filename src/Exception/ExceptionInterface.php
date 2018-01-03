9771c0ed4951c35419a63f1968f24ed47c7f9867
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-03 00:20:01 -0500

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,8 +1,13 @@
 <?php
-/*
- * @link      <https://github.com/Genial-Framework/Validator> for the canonical source repository
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Validator> for the canonical source repository.
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Validator/blob/master/LICENSE> New BSD License
+ * @license   <https://github.com/Genial-Framework/Validator/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Validator\Exception;
@@ -12,4 +17,5 @@ namespace Genial\Validator\Exception;
  */
 interface ExceptionInterface
 {
+  
 }
