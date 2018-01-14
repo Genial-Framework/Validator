3c4e94589a3a1b2cfeb9632d1b9c41b2bf23c109
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-14 02:32:43 -0500

    Add files via upload

---
+++ Exception/OutOfBoundsException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link      <https://github.com/SyzerPHP/Validator> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <https://github.com/SyzerPHP/Validator/blob/master/LICENSE> New BSD License.
+ */
+namespace Syzer\Validator\Exception;
+/**
+ * OutOfBoundsException.
+ */
+class OutOfBoundsException extends \OutOfBoundsException implements ExceptionInterface
+{
+}
