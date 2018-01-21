52ced9d0e406ff03a14ba692e127e91512588e44
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-21 18:36:41 -0500

    Create ValidatorConfig.php

---
+++ src/Bin/ValidatorConfig.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is a part of LaxovePHP.
+ *
+ * @author Nicholas English       <https://github.com/Nenglish7>.
+ * @author LaxovePHP Contributors <https://github.com/orgs/LaxovePHP/people>.
+ *
+ * @copyright 2017-2018 LaxovePHP.
+ *
+ * @link    <https://github.com/LaxovePHP/Validator> Github Repository.
+ * @license <https://github.com/LaxovePHP/Validator/blob/master/LICENSE> New BSD License.
+ */
+namespace LaxovePHP\Validator;
+/**
+ * ValidatorConfig.
+ */
+class ValidatorConfig
+{
+    /**
+     * @const array AVALIABLE_VALIDATORS The list of avaliable validators.
+     */
+    const AVALIABLE_VALIDATORS = [
+        /*...*/
+    ]
+}
