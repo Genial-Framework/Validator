74021a4a24c9dbe46debaab501f0c12848a2df18
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-21 18:32:04 -0500

    Create ValidatorInterface.php

---
+++ src/Bin/ValidatorInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is a part of LaxovePHP.
+ *
+ * @author Nicholas English       <https://github.com/Nenglish7>.
+ * @author LaxovePHP Contributors <https://github.com/orgs/LaxovePHP/people>.
+ *
+ * @copyright 2017-2018 LaxovePHP.
+ *
+ * @link    <https://github.com/LaxovePHP/Validator> Github Repository.
+ * @license <https://github.com/LaxovePHP/Validator/blob/master/LICENSE> New BSD License.
+ */
+namespace LaxovePHP\Validator;
+/**
+ * ValidatorInterface.
+ */
+class Handler implements HandlerInterface
+{
+    public function reportErrors(string $type): array;
+    public function validate($testCase): bool;
+}
