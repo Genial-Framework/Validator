0be4d47a2ab0d8f54d5fd22b3cec89ce5b4a84f1
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-23 15:32:03 -0500

    Update AbstractValidator.php

--- src/AbstractValidator.php
+++ src/AbstractValidator.php
@@ -5,7 +5,7 @@
  * @author Nicholas English <https://github.com/Nenglish7>
  * @author LaxovePHP Contributors <https://github.com/orgs/LaxovePHP/people>
  *
- * @copyright Copyright (c) 2018 LaxovePHP. <https://github.com/LaxovePHP>
+ * @copyright Copyrght (c) 2018 LaxovePHP. <https://github.com/LaxovePHP>
  * @license   <https://github.com/LaxovePHP/Validator/blob/master/LICENSE> New BSD License
  */
 namespace LaxovePHP\Validator;
