f7a2e3f021fd13c66810ebaad1b7aa2fb93ce990
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-23 22:10:15 -0500

    Update boot.php

--- boot.php
+++ boot.php
@@ -5,4 +5,5 @@
  * @link    <https://github.com/genial-framework/Validator> Github Repository.
  * @license <https://github.com/genial-framework/Validator/blob/master/LICENSE> New BSD License.
  */
+
 define('VALIDATOR_VERSION', '1.0.0');
