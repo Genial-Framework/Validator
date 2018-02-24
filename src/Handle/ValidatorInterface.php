9c1357ba176386e7f70dd1f2a25f87f632468e87
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-23 22:17:43 -0500

    Update ValidatorInterface.php

--- src/Handle/ValidatorInterface.php
+++ src/Handle/ValidatorInterface.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * This file is a part of Genial Validator.
+ *
+ * @link    <https://github.com/genial-framework/Validator> Github Repository.
+ * @license <https://github.com/genial-framework/Validator/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Validator\Handle;
+
+/**
+ * ValidatorInterface.
+ */
+interface ValidatorInterface
+{
+}
